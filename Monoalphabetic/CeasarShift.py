0984b6f3be150640b10b5a93f7a286dcd4ba329a
Bobby Stiles <moviestiles@gmail.com>
2014-01-12 22:37:25 -0800

    Updated documentation on Additive and Multiplicative Ciphers

--- Monoalphabetic/CeasarShift.py
+++ Monoalphabetic/CeasarShift.py
@@ -1,15 +1,34 @@
+###
+# Caesar Shift
+#  A Caesar Shift, otherwise known as an Additive Cipher,
+#  takes the letters in a message and shifts them through the alphabet by some integer amount.
+#
+# Encryption Example: Key = 4
+#  craze = 2 17 0 25 4 becomes 6 21 4 29 8 becomes 6 21 4 3 8 = gvedi
+#  Any number greater than the index of Z loops back around, otherwise known as mod 26.
+#
+# Decryption is easy.  Simply subtract the encryption key instead of adding.
+# You can also add the additive inverse of the key.
+# From the previous example, -4 or 22 would be the decryption key.
+###
+
+###
+# Additive Inverse
+#  The additive inverse of a number, x, is another number, y, where x + y = 0.
+#  In normal numbers, this is just simply the negative version.
+#  In cases like this, we're only dealing with the numbers 0-25, so this changes slightly.
+#  x + y = 0 still holds true, but now all numbers are mod 26, so 4 + 22 = 0
+###
+
 import string
 
 ##
 #caesarShift
 #Description: Perform a Caesar Shift on a given message to either encrypt or decrypt the message.
-# This is also known as an Additive Cipher
-# A Caesar Shift takes the letters in a message and shifts them through the alphabet by some integer amount.
-# Encryption shifts to the right, while decryption shifts to the left.
 #
 #Parameters:
 #   message - The text to shift
-#   key     - The integer amount to shift the message by
+#   key     - The integer amount to shift the message by.  Assumed to be the encryption key.
 #   encrypt - Whether to encrypt the message or decrypt it
 #
 #Return: The new text after shifting
@@ -46,10 +65,15 @@ def caesarShiftStringOps(message, key, encrypt = True):
     shiftedAlphabet = alphabet[key:] + alphabet[:key]
     return message.translate(string.maketrans(alphabet, shiftedAlphabet))
 
-print caesarShift("Test message", 1)
-print caesarShift("uftunfttbhf", 1, False)
-print caesarShiftStringOps("Test message", 1)
-print caesarShiftStringOps("uftunfttbhf", 1, False)
+##
+#test
+#Description: A small method to test the outputs of the methods in this file.
+##
+def test():
+    print caesarShift("Test message", 1)
+    print caesarShift("uftunfttbhf", 1, False)
+    print caesarShiftStringOps("Test message", 1)
+    print caesarShiftStringOps("uftunfttbhf", 1, False)
 
 #Output:
 #uftunfttbhf
--- Monoalphabetic/MultiplicativeCipher.py
+++ Monoalphabetic/MultiplicativeCipher.py
@@ -1,9 +1,48 @@
+###
+# Multiplicative Cipher
+#  A multiplicative cipher is similar to the Caesar Shift, except instead of adding
+#  to the alphabetic index, the index is multiplied.
+#
+#  Because all of these numbers are mod 26, that means that one result can be shared by multiple
+#  multiplications.  For example:
+#  4*1 = 4 and 4*14 = 56 = 4
+#  This is bad since we don't want multiple letters mapping to one letter, since this will make
+#  decryption a nightmare, if not impossible.
+#  Therefore, only certain integers are ok to use as keys.  Only integers which are coprime with 26
+#  make valid keys.  The list of valid keys are: 3 5 7 9 11 15 17 19 21 23 25
+#
+# Encryption Example: Key = 3 and assuming A = 0
+#  craze = 2 17 0 25 4 becomes 6 51 0 75 12 becomes 6 25 0 23 12 = gzaxm
+#
+# Decryption:
+#  To find the decryption key, you must find the multiplicative inverse of the encryption key.
+#  The multiplicative inverse of 3 is 9.
+#  gzaxm = 6 25 0 23 12 becomes 54 225 0 207 108 becomes 2 17 0 25 4 = craze
+###
+
+###
+# Coprime
+#  Two numbers are coprime when their gcd = 1
+###
+
+###
+# gcd
+#  GCD stands for Greatest Common Divider.  It is the highest number that can evenly divide multiple numbers.
+#  A very common algorithm for finding the gcd of two numbers is the Euclidean Algorithm.
+###
+
+###
+# Multiplicative Inverse
+#  The multiplicative inverse of a number, x, is another number, y, such that x*y = 1
+#  In the mod 26 world, for example, 3*9 = 27 = 1
+#  Not all integers here have a multiplicative inverse, so not all 25 integers can be keys.
+#  For example, no possible number can be used for x such that 4*x mod 26 = 1
+###
+
 from MultiplicativeKeys import multiplicativeKeyCount
 import string
 
-############################################
 ##   Finding the multiplicative inverse   ##
-############################################
 
 #Straight from https://en.wikibooks.org/wiki/Algorithm_Implementation/Mathematics/Extended_Euclidean_algorithm
 
@@ -47,12 +86,11 @@ def modinv(a, m):
 ##
 #multiplicativeCipher
 #Description: Either encrypt or decrypt a message with a multiplicative cipher
-# with a given key.  The key is assumed to be the encryption key.
-# This function assumes that alphabet indexes start at 0.
+# with a given key.  This function assumes that alphabet indexes start at 0.
 #
 #Parameters:
 #   message - The message to be encrypted or decrypted
-#   key - The key to use with the cipher
+#   key - The key to use with the cipher.  Assumed to be the encryption key.
 #   encrypt - True if encrypting, False otherwise
 #
 #Return: The new message
@@ -78,6 +116,11 @@ def multiplicativeCipherA0(message, key, encrypt = True):
 
     return newMessage
 
+##
+#testA0
+#Description: A small method to test the outputs of the methods in this file.
+# Specifically the ciphering method that assumes the index of A is 0.
+##
 def testA0():
     print multiplicativeCipherA0("This is a test message", 5)
     print multiplicativeCipherA0("rjomomarumriummaeu", 5, False)
