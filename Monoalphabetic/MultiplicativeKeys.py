0fd338192e7edd3886aadcd9af5430bc6e7f3721
Bobby Stiles <moviestiles@gmail.com>
2014-01-13 10:02:03 -0800

    Added a second function just for calculating the list of valid keys, and added public list variable for the 26-letter alphabet list of keys.

--- Monoalphabetic/MultiplicativeKeys.py
+++ Monoalphabetic/MultiplicativeKeys.py
@@ -1,18 +1,28 @@
+###
+# For any definitions you may need in this file, look in MultiplicativeCipher.py
+###
+
 from fractions import gcd
 
 ##
+# A list of the normal 26-letter alphabet keys.
+# No need to repeatedly calculate such a common list.
+##
+normAlphabetKeys = [3, 5, 7, 9, 11, 15, 17, 19, 21, 23, 25]
+
+##
 #multiplcativeKeyCount
-#Description: Given a length of some alphabet, calculate how many valid multiplicative cipher keys that alphabet can have.
-# A key is valid if it is an integer less than the alphabet length and coprime with the alphabet length.
-# Two numbers are coprime if the gcd of the two numbers is 1.
+#Description: Given a length of some alphabet, calculate how many
+# valid multiplicative cipher keys that alphabet can have.
 #
 #Parameters:
 #   alphabetLength - The length of the alphabet
-#   showList - Whether or not to actually keep track of the list of keys
+#   keepList - Whether or not to actually keep track of the list of keys
 #
-#Return: A tuple of the key count and the array of key values. The second value is None if showList is False.
+#Return: A tuple of the key count and the array of key values.
+# The second value is None if keepList is False.
 ##
-def multiplicativeKeyCount(alphabetLength, showList = True):
+def multiplicativeKeyCount(alphabetLength, keepList = True):
     keyCount = 0
     keyList = []
     #Start at 2 because 1 and 0 are never valid keys
@@ -21,14 +31,38 @@ def multiplicativeKeyCount(alphabetLength, showList = True):
         if gcd(i, alphabetLength) == 1:
             keyCount += 1
             #Add to the list of keys if we're keeping track of that
-            if showList:
+            if keepList:
                 keyList.append(i)
 
-    if showList:
+    if keepList:
         return keyCount, keyList
     else:
         return keyCount, None
 
+##
+#getMultiplcativeKeys
+#Description: Given a length of some alphabet, calculate all the
+# valid multiplicative cipher keys that alphabet can have.
+#
+#Parameters:
+#   alphabetLength - The length of the alphabet
+#
+#Return: A list of all the key values.
+##
+def multiplicativeKeyCount(alphabetLength):
+    keyList = []
+    #Start at 2 because 0 and 1 are never valid keys
+    for i in range(2, alphabetLength):
+        #Keep the key values if the gcd of the two numbers is one.
+        if gcd(i, alphabetLength) == 1:
+            keyList.append(i)
+
+    return keyList
+
+##
+#test
+#Description: A small method to test the outputs of the methods in this file.
+##
 def test():
     keys = multiplicativeKeyCount(26)
     print "Number of keys: ", keys[0]
