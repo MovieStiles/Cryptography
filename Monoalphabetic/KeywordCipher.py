972b68517ec4ad6948439e9e5a56e8c3d86034b0
Bobby Stiles <moviestiles@gmail.com>
2014-01-15 19:30:01 -0800

    Finished work on KeywordCipher.  Decided to just add another parameter to the existing method for the variant cipher instead of creating a new method.

--- Monoalphabetic/KeywordCipher.py
+++ Monoalphabetic/KeywordCipher.py
@@ -26,47 +26,6 @@
 # to try and deduce what the letter correspondences are and what the keyword might be.
 ###
 
-import string
-
-##
-#keywordCipher
-#Description: Given a message and keyword, encrypt or decrypt a message
-# using a keyword cipher.
-#
-#Paramters:
-#   message - The message to encrypt or decrypt.
-#   keyword - The keyword used in the cipher.
-#   encrypt - True if encrypting, False otherwise.  True by default.
-#
-#Return: The resulting message.
-##
-def keywordCipher(message, keyword, encrypt = True):
-    message = message.lower().replace(' ', '')
-    alphabet = string.lowercase
-
-    #First, remove repeated characters from the keyword.
-    #If anyone reading this can do it faster than O(n^2), let me know.
-    keyword = ''.join(sorted(set(keyword), key = keyword.index))
-
-    #Then build the new alphabet.
-    newAlpha = keyword + ''.join(sorted(set(alphabet).difference(keyword)))
-
-    #Now construct the new message
-
-    #Normally, this would be the way to go:
-    #for char in message:
-    #    if encrypt:
-    #        newMessage += newAlpha[alphabet.find(char)]
-    #    else:
-    #        newMessage += alphabet[newAlpha.find(char)]
-    #
-    #But that's just to make what's going on more clear.  I think we can move on
-    # for effeciency's sake by now.
-    if encrypt:
-        return message.translate(string.maketrans(alphabet, newAlpha))
-    else:
-        return message.translate(string.maketrans(newAlpha, alphabet))
-
 ###
 # Another variant exists which also puts an additive cipher on top of the keyword cipher.
 # In this case, the cipher is as follows:
@@ -83,3 +42,48 @@ def keywordCipher(message, keyword, encrypt = True):
 #  1. crazytrain -> crazytin
 #  2. D -> c   E -> r   F -> a.... and so on.
 ###
+
+import string
+
+##
+#keywordCipher
+#Description: Given a message and keyword, encrypt or decrypt a message
+# using a keyword cipher.
+#
+#Paramters:
+#   message - The message to encrypt or decrypt.
+#   keyword - The keyword used in the cipher.
+#   keyShift - The amount to shift the new alphabet created by the keyword by.  (default: 0)
+#   encrypt - True if encrypting, False otherwise.  (default: True)
+#
+#Return: The resulting message.
+##
+def keywordCipher(message, keyword, keyShift = 0, encrypt = True):
+    message = message.lower().replace(' ', '')
+    alphabet = string.lowercase
+
+    #First, remove repeated characters from the keyword.
+    #If anyone reading this can do it faster than O(n^2), let me know.
+    keyword = ''.join(sorted(set(keyword), key = keyword.index))
+
+    #Then build the new alphabet.
+    newAlpha = keyword + ''.join(sorted(set(alphabet).difference(keyword)))
+    #Then shift the new alphabet if there's any shifting to do.
+    if not keyShift == 0:
+        newAlpha = newAlpha[keyShift:] + newAlpha[:keyShift]
+
+    #Now construct the new message
+
+    #Normally, this would be the way to go:
+    #for char in message:
+    #    if encrypt:
+    #        newMessage += newAlpha[alphabet.find(char)]
+    #    else:
+    #        newMessage += alphabet[newAlpha.find(char)]
+    #
+    #But that's just to make what's going on more clear.  I think we can move on
+    # for efficiency sake by now.
+    if encrypt:
+        return message.translate(string.maketrans(alphabet, newAlpha))
+    else:
+        return message.translate(string.maketrans(newAlpha, alphabet))
