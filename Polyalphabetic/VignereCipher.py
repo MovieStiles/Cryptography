b1095bccd2ad07196b6e50a7ab1e130f566327c9
Bobby Stiles <moviestiles@gmail.com>
2014-01-25 09:43:13 -0800

    Added a function to VignereCipher to guess at the keyword letters given a length.

--- Polyalphabetic/VignereCipher.py
+++ Polyalphabetic/VignereCipher.py
@@ -33,7 +33,7 @@
 #  These tests are outlined in KeywordLengthTests.py.
 ###
 
-import string
+from LetterFrequency import calcLetterFrequency
 
 ##
 #vignereCipherEncrypt
@@ -109,6 +109,37 @@ def messageSplit(message, keywordLength):
             subMessages.append(tempString)
 
     return subMessages
+
+##
+#analyzeSubMessages
+#Description: Given a ciphertext message encrypted with the Vignere Cipher
+# and a possible keyword length, perform frequency analysis on each section
+# of the message that is encoded with the same letter in the keyword.
+#
+#Parameters:
+#   ciphertext - The ciphertext to analyze
+#   kwLength - A possible length of the encryption keyword
+#
+#Return: A list of the top three most likely letters of the keyword for
+# each submessage.
+##
+def analyzeSubMessages(ciphertext, kwLength):
+    subs = messageSplit(ciphertext, kwLength)
+    possibleLetters = []
+
+    #Loop through the submessages
+    for sub in subs:
+        freqs = calcLetterFrequency(sub)
+        topThree = []
+        #Loop through the top 3 most frequent letters
+        for i in range(0, 3):
+            if freqs[i] is not None:
+                key = (26 - (ord(freqs[i]) - 97)) % 26
+                topThree.append(chr(key + 97))
+
+        possibleLetters.append(topThree)
+
+    return possibleLetters
 
 def test():
     print vignereCipherEncrypt("bluecheese", "eat")
